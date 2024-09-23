53b46afae94a6b9cbdf583e77e3124b49e8f79c4
Alex Guzey <aleksandar.guzei@yandex.ru>
2024-09-23 22:01:40 +0300

    Добавил обработчик и логирование

--- app/main.py
+++ app/main.py
@@ -7,24 +7,43 @@ from bot.callbacks import router as callback_router
 from bot.fsm_context import router as fsm_context_router
 
 
+# Настройка логирования
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(),  # Логирование в консоль
+        logging.FileHandler("bot.log", encoding='utf-8')  # Логирование в файл
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+
 async def main() -> None:
     """Запуск SCID бота."""
 
     try:
         check_token()
     except ValueError as e:
-        print(e)
+        logger.error(f"Ошибка проверки токена: {e}")
         return
 
     dispatcher.include_router(message_router)
     dispatcher.include_router(callback_router)
     dispatcher.include_router(fsm_context_router)
 
-    await dispatcher.start_polling(bot)
+    try:
+        logger.info("Запуск бота...")
+        await dispatcher.start_polling(bot)
+    except Exception as e:
+        logger.error(f"Критическая ошибка в работе бота: {e}")
+
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print('Exit')
+        logger.info("Бот остановлен пользователем.")
+    except Exception as e:
+        logger.error(f"Произошла непредвиденная ошибка: {e}")
