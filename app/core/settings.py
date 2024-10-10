f73cb404f159e72521f869f89cd3e674192b3ab6
Kseniya Tetercheva <snegurka.tkd@gmail.com>
2024-10-10 18:54:40 +0300

    update env file

--- app/core/settings.py
+++ app/core/settings.py
@@ -10,6 +10,8 @@ class Settings(BaseSettings):
     postgres_user: str
     postgres_password: str
     postgres_db: str
+    db_host: str
+    db_port: str
 
     class Config:
         env_file = '.env'
