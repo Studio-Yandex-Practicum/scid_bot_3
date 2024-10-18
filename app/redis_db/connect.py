8c43299809137cc2ed3c3afefce9a7ddafa40bed
Kseniya Tetercheva <snegurka.tkd@gmail.com>
2024-10-18 16:59:01 +0300

    update radis host

--- app/redis_db/connect.py
+++ app/redis_db/connect.py
@@ -5,5 +5,5 @@ async def get_redis_connection():
     """Подключение к Redis."""
 
     return await aioredis.from_url(
-        "redis://localhost:6379", decode_responses=True
+        "redis://158.160.77.163:6379", decode_responses=True  # localhost:
     )
